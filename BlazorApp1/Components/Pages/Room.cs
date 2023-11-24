88d950d867bcb2fa439e5bb17eea375442d5fada
Oache <lukas.aichhorn07@gmail.com>
2023-11-24 16:13:35 +0100

    Initial Commit

---
+++ BlazorApp1/Components/Pages/Room.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorApp1;
+
+public class Room
+{
+    public int number { get; set; }
+    public string Type { get; set; }
+    public int Capacity { get; set; }
+    public bool Occupied { get; set; }
+    public bool Cleaned { get; set; }
+
+    public Room(int number, string type, int capacity, bool occupied, bool cleaned)
+    {
+        this.number = number;
+        Type = type;
+        Capacity = capacity;
+        Occupied = occupied;
+        Cleaned = cleaned;
+    }
+}
